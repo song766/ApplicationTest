2fc3a1f46e6ca48ae27eb6028b9da1e12cbe519e
SongEun <766song@gmail.com>
2021-09-01 15:53:46 +0900

    띄어쓰기 수정 commit

--- src/test/java/junit/CalcUtill/test/SearchMaxTest.java
+++ src/test/java/junit/CalcUtill/test/SearchMaxTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import com.green.biz.CalcUtill;
 
 public class SearchMaxTest {
-
 	@Test
 	public void testSearchMax() {
 		CalcUtill calc = new CalcUtill();
@@ -15,5 +14,4 @@ public class SearchMaxTest {
 		assertEquals("CalcUtill Max", 4, calc.searchMax(new int[] {1, 3, 4, 2}));
 		assertEquals("CalcUtill Max", -1, calc.searchMax(new int[] {-12, -1, -3, -4, -2}));
 	}
-
 }

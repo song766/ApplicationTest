cbdad12112221c58d775e4808713d79c7558d0a9
SongEun <766song@gmail.com>
2021-09-01 15:35:19 +0900

    ApplicationTest 프로젝트 github에 Commit and Push!

---
+++ src/test/java/junit/CalcUtill/test/SearchMaxTest.java
@@ -0,0 +1,19 @@
+package junit.CalcUtill.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.green.biz.CalcUtill;
+
+public class SearchMaxTest {
+
+	@Test
+	public void testSearchMax() {
+		CalcUtill calc = new CalcUtill();
+		
+		assertEquals("CalcUtill Max", 4, calc.searchMax(new int[] {1, 3, 4, 2}));
+		assertEquals("CalcUtill Max", -1, calc.searchMax(new int[] {-12, -1, -3, -4, -2}));
+	}
+
+}
